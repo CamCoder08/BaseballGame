79bfe88e4c75161fa7c8532416630bbdb3ad5252
CamCoder08 <y30281@gmail.com>
2025-03-21 10:27:55 +0900

    feat: 랜덤 숫자 생성 기능 추가
    - makeAnswer() 메서드를 추가하여 1~9 사이의 서로 다른 랜덤한 3자리 숫자를 생성하는 기능을 구현함.
    - while 반복문을 사용하여 중복이 없는 숫자를 리스트에 추가하는 방식으로 동작하도록 설계함.
    - Int.random(in: 1...9)를 활용하여 난수를 생성함.

--- BaseballGame_JoonYoung/BaseballGame_JoonYoung/main.swift
+++ BaseballGame_JoonYoung/BaseballGame_JoonYoung/main.swift
@@ -6,6 +6,17 @@ class BaseballGame {
         print("||| 숫자 야구게임을 시작하겠습니다! 룰루랄라~ |||")
         play() // 게임 진행 함수 호출
     }
+    // 랜덤한 3개의 숫자를 생성하는 함수 (중복 없이 숫자 배열 반환)
+    private func makeAnswer() -> [Int] {
+        var numbers: [Int] = []
+        while numbers.count < 3 { // 배열에 숫자가 3개가 될 때까지 반복
+            let randomNumber = Int.random(in: 1...9)
+            if !numbers.contains(randomNumber) { // 중복 확인
+                numbers.append(randomNumber) // 중복이 없으면 배열에 추가
+            }
+        }
+        return numbers  // 3개의 랜덤 숫자가 포함된 배열 반환
+    }
 
 
 
